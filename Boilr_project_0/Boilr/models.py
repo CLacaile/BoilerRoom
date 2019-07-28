6c8a5d067ffae491e2bb7136e1df1d63be68071b
CLacaile <clementlacaile@gmail.com>
2019-07-28 19:54:55 +0200

    initial commit of classes, no test

--- Boilr_project_0/Boilr/models.py
+++ Boilr_project_0/Boilr/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Room(models.Models):
+    name = models.CharField(max_length=32)
+    type = models.CharField(max_length=32)
+    surface = models.FloatField()
+
+class Sensor(models.Models):
+    name = models.CharField(max_length=32)
+    room = models.ForeignKey('Room', on_delete=models.SET_NULL)
+
+class Measure(models.Models):
+    value = models.FloatField()
+    unit = models.CharField(max_length=16)
+    time = models.DataTimeField(auto_now=False, auto_now_add=True)
+    sensor = models.ForeignKey('Sensor', on_delete=models.CASCADE)
