a31dce996fd0f1b138c4d4b2c0af3c9059b3299b
CLacaile <clementlacaile@gmail.com>
2019-08-11 17:07:50 +0200

    added new Boilr/room/<id> and Boilr/room/<id>/stats paths in urlpatterns

--- Boilr_project_0/Boilr/urls.py
+++ Boilr_project_0/Boilr/urls.py
@@ -1,6 +1,13 @@
 from django.urls import path
 from . import views
 
+app_name = 'Boilr'
 urlpatterns = [
+        # ex : /boilr/
 	path('', views.index, name='index'),
+        # ex : boilr/room/1
+        path('room/<int:room_id>/', views.detail, name='detail'),
+        # ex : boilr/room/1/stats
+        path('room/<int:room_id>/stats', views.stats, name='stats'),
+
 ]
