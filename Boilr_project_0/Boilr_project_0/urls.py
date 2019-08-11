a483702cfe89b706e9ba1484b9757f9d22daf1ea
CLacaile <clementlacaile@gmail.com>
2019-08-11 17:04:44 +0200

    changed urlpatterns from boilr to Boilr

--- Boilr_project_0/Boilr_project_0/urls.py
+++ Boilr_project_0/Boilr_project_0/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('boilr/', include('Boilr.urls')),
+    path('Boilr/', include('Boilr.urls')),
     path('admin/', admin.site.urls),
 ]
