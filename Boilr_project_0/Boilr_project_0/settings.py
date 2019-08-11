5f17c63d51fa1f614eea261a253765cc39e3481f
CLacaile <clementlacaile@gmail.com>
2019-08-11 11:43:41 +0200

    added Boilr app in INSTALLED_APP

--- Boilr_project_0/Boilr_project_0/settings.py
+++ Boilr_project_0/Boilr_project_0/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ['raspberrypi']
 # Application definition
 
 INSTALLED_APPS = [
+    'Boilr.apps.BoilrConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
