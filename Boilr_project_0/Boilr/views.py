5f8ac820483ac0e3251edbcfc352a52b9dfd2eb0
CLacaile <clementlacaile@gmail.com>
2019-08-11 17:08:49 +0200

    views.py reflects end of tutorial 3 of django 2.2 doc

--- Boilr_project_0/Boilr/views.py
+++ Boilr_project_0/Boilr/views.py
@@ -1,8 +1,26 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404, get_list_or_404
+from django.http import HttpResponse, Http404
+from django.template import loader
+from .models import Room
 
-
-# Create your views here.
-
+# home page
 def index(request):
-	return HttpResponse("Hello, world. You're at the polls index.")
+    room_list = get_list_or_404(Room)
+    context = {
+            'room_list': room_list,
+    }
+    return render(request, 'Boilr/index.html', context)
+
+# room details
+def detail(request, room_id):
+   # try:
+   #     room = Room.objects.get(id=room_id)
+   # except Room.DoesNotExist:
+   #     raise Http404("Room does not exist")
+    room = get_object_or_404(Room, id=room_id)
+    return render(request, 'Boilr/detail.html', {'room': room})
+
+# room stats
+def stats(request, room_id):
+    return HttpResponse("You're looking at the stats of room %s" % room_id)
+
